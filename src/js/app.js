fbbf119397159c9fcf9a268d65de1057b4f69538
wangrui <zxcvb2850@163.com>
2017-11-23 18:51:30 +0800

    a lot

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
-
 import $ from 'jquery';
 import 'bootstrap';
+
+$(function () {
+
+    /*产品列表页菜单点击*/
+    let menuItem = $('.accordion-toggle');
+    menuItem.on('click', function () {
+        if ($(this).attr('aria-expanded') === 'false') {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    })
+
+})
